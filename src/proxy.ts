296e1d0880c38087f4b6c04bcdade91186f154db
Harshit Saxena <harshit214saxena@gmail.com>
2026-02-22 20:54:46 +0530

    fix: update proxy config to include root path in matcher

--- src/proxy.ts
+++ src/proxy.ts
@@ -21,5 +21,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
+  matcher: ["/","/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
 };
